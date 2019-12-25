6ba72c0d4ffc0653f9673f76c5f3bda9c2702eaa
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-12-25 23:54:46 +0300

    Create Phones.cs

---
+++ Lab5/Lab5/Models/Phones.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab5.Models
+{
+    public class Phones
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Brend { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
