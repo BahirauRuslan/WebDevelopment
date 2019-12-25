23a3bd4a56061305107122d6f2813fe055917597
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-12-25 23:54:42 +0300

    Create PhonesContext.cs

---
+++ Lab5/Lab5/Models/PhonesContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lab5.Models
+{
+    public class PhonesContext : DbContext
+    {
+        public DbSet<Phones> Phones { get; set; }
+
+        public PhonesContext(DbContextOptions<PhonesContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
