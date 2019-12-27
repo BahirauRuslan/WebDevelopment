46abe32f1c57c0cf7e5ad4eb54825375783f4612
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-12-27 20:04:44 +0300

    Update Phones.cs

--- Lab5/Lab5/Models/Phones.cs
+++ Lab5/Lab5/Models/Phones.cs
@@ -1,13 +1,19 @@
-﻿namespace Lab5.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab5.Models
 {
     public class Phones
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Enter phone name")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Enter phone brend")]
         public string Brend { get; set; }
 
+        [Range(0.00, 9999999.99, ErrorMessage = "Price must be correct")]
+        [Required(ErrorMessage = "Enter phone price")]
         public decimal Price { get; set; }
     }
 }
